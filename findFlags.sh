279fb57f5c6251ed5342425b2c8ddbad02be1ca3
FreddiPhish <62122430+FreddiPhish@users.noreply.github.com>
2022-01-04 21:49:25 +1100

    Create findFlags.sh

---
+++ findFlags.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Find files called flag.txt
+
+#Find strings that look like flags in txt files
+
+#For faster results, search home directories, web directoriess, /opt, then all directories
+
+#Look inside any docker containers
+
